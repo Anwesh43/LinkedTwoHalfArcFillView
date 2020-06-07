7e910c00738002d9515eebcf74e0bc4e9ccccb2a
Anwesh43 <anweshthecool0@gmail.com>
2020-06-08 05:10:05 +0530

    adding constants and libraries

--- twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
+++ twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.twosidehalfarcfillview
 /**
  * Created by anweshmishra on 08/06/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val arcs : Int = 2
+val parts : Int = 3
+val scGap : Float = 0.2f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20

031135d965f6ffa6df7fb5844ef805bf172e5513
Anwesh43 <anweshthecool0@gmail.com>
2020-06-08 05:23:58 +0530

    adding Canvas extension methods

--- twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
+++ twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
@@ -22,8 +22,39 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val rot : Float = 180f
+val start : Float = -90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTwoSideHalfArc(i : Int, scale : Float, w : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    scale(1f - 2 * i, 1f)
+    translate((w / 2 - size) * (1 - sf2), 0f)
+    drawArc(RectF(-size, -size, size, size), start, rot * sf1, false, paint)
+    drawArc(RectF(-size, -size, size, size), start, rot * sf3, true, paint)
+    restore()
+}
+
+fun Canvas.drawTSHANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    for (j in 0..1) {
+        drawTwoSideHalfArc(j, scale, w, size, paint)
+    }
+    restore()
+}

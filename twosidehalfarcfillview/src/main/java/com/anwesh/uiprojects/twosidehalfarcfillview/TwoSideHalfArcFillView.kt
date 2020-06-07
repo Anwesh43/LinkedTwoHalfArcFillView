645d29aa052b717e4cff635fc61c0ee02eb2c183
Anwesh43 <anweshthecool0@gmail.com>
2020-06-08 05:25:04 +0530

    adding TwoSideHalfArcFillView class

--- twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
+++ twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawTSHANode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TwoSideHalfArcFillView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

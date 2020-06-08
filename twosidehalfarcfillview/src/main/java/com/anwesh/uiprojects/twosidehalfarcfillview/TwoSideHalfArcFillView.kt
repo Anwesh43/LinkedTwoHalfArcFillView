cab33669e65653a3b0a6addc72434860f76a7cb5
Anwesh43 <anweshthecool0@gmail.com>
2020-06-08 05:55:29 +0530

    adding TwoSideHalfArcFill class

--- twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
+++ twosidehalfarcfillview/src/main/java/com/anwesh/uiprojects/twosidehalfarcfillview/TwoSideHalfArcFillView.kt
@@ -163,4 +163,28 @@ class TwoSideHalfArcFillView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TwoSideHalfArcFill(var i : Int) {
+
+        private val root : TSHANode = TSHANode(0)
+        private var curr : TSHANode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

492d26fb75a6a3b3830c017fc6a13752a7597ef8
Anwesh43 <anweshthecool0@gmail.com>
2020-06-08 06:25:02 +0530

    adding fullscreen support and view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtwosidehalfarcfillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtwosidehalfarcfillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedtwosidehalfarcfillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.twosidehalfarcfillview.TwoSideHalfArcFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TwoSideHalfArcFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
